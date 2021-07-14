b1da59c17c5ba212108b6a4245b11f7c493c16d7
Z <zeta.hoffman@protonmail.com>
2021-07-14 02:51:30 -0600

    Configure NetworkInterfaces Variables to work
    Generate Fedora Network Interfaces variables to Ethernet, Wireless, Bluetooth and USB, rebuild with the variables and add comment with Debian Variables

--- bin/copyLocal.sh
+++ bin/copyLocal.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
-ETHERIP=$(/usr/sbin/ifconfig enp0s25 | grep 'inet ' | awk '{print $2}')
+
+#This Script gets the active interface and copy it to clipboard
+#needs xclip
+
+#region Interfaces
+EthernetInterface='enp0s25' #In Debian base eth0
+USBInterface='enp0s20u3u3' #In Debian base usb0
+WirelessInterface='wlo1' #In Debian base wlan0
+BluetoothInterface='bnep0' #Same in Debian base
+#end region
+
+
+ETHERIP=$(/usr/sbin/ifconfig $EthernetInterface | grep 'inet ' | awk '{print $2}')
 
 if [ "$ETHERIP" = "" ]; then
-	# Change usb0 for your USB interface
-	USBIP=$(/usr/sbin/ifconfig usb0 2>/dev/null)
-	if [ $(echo $?) != "1" ]; then
-		echo -n $(/usr/sbin/ifconfig usb0 | grep 'inet ' | awk '{print $2}') | xclip -sel clipboard
+        # Change usb0 for your USB interface
+        USBIP=$(/usr/sbin/ifconfig  $USBInterface 2>/dev/null)
+        if [ $(echo $?) = "0" ]; then
+		echo -n $(/usr/sbin/ifconfig $USBInterface | grep 'inet ' | awk '{print $2}') | xclip -sel clipboard
 	else
 		# Change bnep0 for your Bluetooth lan interface
-		BLUETOOTHIP=$(/usr/sbin/ifconfig bnep0 2>/dev/null)
-		if [ $(echo $?) != "1" ]; then
-			echo -n $(/usr/sbin/ifconfig bnep0 | grep 'inet ' | awk '{print $2}') | xclip -sel clipboard
+		BLUETOOTHIP=$(/usr/sbin/ifconfig $BluetoothInterface 2>/dev/null)
+		if [ $(echo $?) = "0" ]; then
+			echo -n $(/usr/sbin/ifconfig $BluetoothInterface | grep 'inet ' | awk '{print $2}') | xclip -sel clipboard
 		else
 			# Change your wireless interface
-			WLAN=$(/usr/sbin/ifconfig wlo1 | grep 'inet ' 2>/dev/null)
+			WLAN=$(/usr/sbin/ifconfig $WirelessInterface | grep 'inet ' 2>/dev/null)
 			if [ $(echo $?) = "0" ]; then
-				echo -n $(/usr/sbin/ifconfig wlo1 | grep 'inet ' | awk '{print $2}') | xclip -sel clipboard
+				echo -n $(/usr/sbin/ifconfig $WirelessInterface | grep 'inet ' | awk '{print $2}') | xclip -sel clipboard
 			else
-				echo "%{F#FF0000}%{u-}%{F#FFFFFF} No Interfaces"
+				echo -n "No Interfaces" | xclip - sel clipboard
 			fi
 		fi
 	fi
--- bin/lan_status.sh
+++ bin/lan_status.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
-ETHERIP=$(/usr/sbin/ifconfig enp0s25 | grep 'inet ' | awk '{print $2}')
+
+
+#The script gets the active NetwokInterface
+
+
+#region Interfaces
+EthernetInterface='enp0s25' #In Debian base eth0
+USBInterface='enp0s20u3u3' #In Debian base usb0
+WirelessInterface='wlo1' #In Debian base wlan0
+BluetoothInterface='bnep0' #Same in Debian base
+#end region
+
+
+ETHERIP=$(/usr/sbin/ifconfig $EthernetInterface | grep 'inet ' | awk '{print $2}')
 
 if [ "$ETHERIP" = "" ]; then
 	# Change usb0 for your USB interface
-	USBIP=$(/usr/sbin/ifconfig usb0 2>/dev/null)
-	if [ $(echo $?) != "1" ]; then
-		echo "%{F#00AAFF}%{u-}%{F#FFFFFF}" $(/usr/sbin/ifconfig usb0 | grep 'inet ' | awk '{print $2}')
+	USBIP=$(/usr/sbin/ifconfig  $USBInterface 2>/dev/null)
+	if [ $(echo $?) = "0" ]; then
+		echo "%{F#00AAFF}%{u-}%{F#FFFFFF}" $(/usr/sbin/ifconfig $USBInterface | grep 'inet ' | awk '{print $2}')
 	else
 		# Change bnep0 for your Bluetooth lan interface
-		BLUETOOTHIP=$(/usr/sbin/ifconfig bnep0 2>/dev/null)
-		if [ $(echo $?) != "1" ]; then
-			echo "%{F#00AAFF}%{u-}%{F#FFFFFF}" $(/usr/sbin/ifconfig bnep0 | grep 'inet ' | awk '{print $2}')
+		BLUETOOTHIP=$(/usr/sbin/ifconfig $BluetoothInterface 2>/dev/null)
+		if [ $(echo $?) = "0" ]; then
+			echo "%{F#00AAFF}%{u-}%{F#FFFFFF}" $(/usr/sbin/ifconfig $BluetoothInterface | grep 'inet ' | awk '{print $2}')
 		else
 			# Change your wireless interface
-			WLAN=$(/usr/sbin/ifconfig wlo1 | grep 'inet ' 2>/dev/null)
+			WLAN=$(/usr/sbin/ifconfig $WirelessInterface | grep 'inet ' 2>/dev/null)
 			if [ $(echo $?) = "0" ]; then
-				echo "%{F#00AAFF}泌%{u-}%{F#FFFFFF}" $(/usr/sbin/ifconfig wlo1 | grep 'inet ' | awk '{print $2}')
+				echo "%{F#00AAFF}泌%{u-}%{F#FFFFFF}" $(/usr/sbin/ifconfig $WirelessInterface | grep 'inet ' | awk '{print $2}')
 			else
 				echo "%{F#FF0000}%{u-}%{F#FFFFFF} No Interfaces"
 			fi
